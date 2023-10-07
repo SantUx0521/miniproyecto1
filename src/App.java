2e48363e09b5f4ee7b035fa406049d52b43b1fc8
NiKolas1828 <nicolas.rodriguez.romero@correounivalle.edu.co>
2023-10-07 15:35:54 -0500

    Diseño menu

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        System.out.println("---------- MENU -----------");
+        System.out.println("[1] Insertar Candidato");
+        System.out.println("[2] Actualizar Candidato");
+        System.out.println("[3] Eliminar Candidato");
+        System.out.println("[4] Buscar Candidato por nombre");
+        System.out.println("[5] Listar todos los Candidato");
+        
+
+
+
+
+
+
+
     }
 }
