e5d1ad94010feb9779a5e156cc2c70ed399ebc67
SantUx0521 <santiago.useche@correounivalle.edu.co>
2023-10-09 18:57:38 -0500

    Agregadas funciones al menú

---
+++ src/Candidato.java
@@ -0,0 +1,38 @@
+public class Candidato extends Ciudadano {
+
+    private String espectro_pol;
+    private String partido_pol;
+
+    public Candidato(String nombre, String ciudad_o, int cedula, String espectro_pol, String partido_pol) {
+        super(nombre, ciudad_o, cedula);
+        this.espectro_pol = espectro_pol;
+        this.partido_pol = partido_pol;
+    }
+    public String getEspectro_pol() {
+        return espectro_pol;
+    }
+
+    public void setEspectropol(String espectro_pol) {
+        this.espectro_pol = espectro_pol;
+    }
+
+    public String getPartido_pol() {
+        return partido_pol;
+    }
+
+    public void setPartido_pol(String partido_pol) {
+        this.partido_pol = partido_pol;
+    }
+    
+
+
+
+
+
+
+}
+
+
+    
+    
+
