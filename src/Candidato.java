2977470604e921803f1d32c36c9171a4e0674d4a
JAlexis1 <144349234+JAlexis1@users.noreply.github.com>
2023-10-12 09:09:12 -0500

    Update Candidato.java

--- src/Candidato.java
+++ src/Candidato.java
@@ -1,56 +1,30 @@
-<<<<<<< HEAD
-=======
 import java.util.ArrayList;
 
->>>>>>> c6a36d0 (Añado la funcionalidad para verificar que la cedula no se repita)
-public class Candidato extends Ciudadano {
+class Candidato {
+    String nombre;
+    String ciudad;
+    int cedula;
+    String partido;
+    int votos;
+    ArrayList<String> propuestas;
 
-    private String espectro_pol;
-    private String partido_pol;
-<<<<<<< HEAD
-=======
-    public String ciudad;
-    public String propuestas;
->>>>>>> c6a36d0 (Añado la funcionalidad para verificar que la cedula no se repita)
-
-    public Candidato(String nombre, String ciudad_o, int cedula, String espectro_pol, String partido_pol) {
-        super(nombre, ciudad_o, cedula);
-        this.espectro_pol = espectro_pol;
-        this.partido_pol = partido_pol;
+    public int getCedula() {
+        return cedula;
     }
-<<<<<<< HEAD
-=======
-    public void Candidaton (String nombre, byte cedula, String ciudad, ArrayList<String> propuestas) {
+    public String getNombre() {
+        return nombre;
     }
->>>>>>> c6a36d0 (Añado la funcionalidad para verificar que la cedula no se repita)
-    public String getEspectro_pol() {
-        return espectro_pol;
+    public Candidato(String nombre, String ciudad, int cedula, String partido, ArrayList<String> propuestas) {
+        this.nombre = nombre;
+        this.ciudad = ciudad;
+        this.cedula = cedula;
+        this.partido = partido;
+        this.propuestas = propuestas;
+        this.votos = 0;
     }
 
-    public void setEspectropol(String espectro_pol) {
-        this.espectro_pol = espectro_pol;
-    }
-
-    public String getPartido_pol() {
-        return partido_pol;
-    }
-
-    public void setPartido_pol(String partido_pol) {
-        this.partido_pol = partido_pol;
-<<<<<<< HEAD
-    }
-=======
-    }}
-    
->>>>>>> c6a36d0 (Añado la funcionalidad para verificar que la cedula no se repita)
-
-
-
-
-<<<<<<< HEAD
 
 }
-
 
 =======
 >>>>>>> c6a36d0 (Añado la funcionalidad para verificar que la cedula no se repita)
